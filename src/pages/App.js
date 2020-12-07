df03ed19c8c2c7fcb72bb0c12a67f19f2ce98a32
Denilson Santos <denilson.avsantos@hotmail.com>
2020-12-07 16:53:15 -0300

    feat:testing data flow

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,27 +1,21 @@
-import React from "react";
-import logo from "../logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: this.props.initItems,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.state.todoItems);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <button onClick={this.handleClick}>Click-me</button>;
   }
 }
 
