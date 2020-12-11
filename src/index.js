3bdcd6a12fd221943d52f219e36172fa0859314d
Denilson Santos <denilson.avsantos@hotmail.com>
2020-12-11 11:29:33 -0300

    feat: todo done

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./pages/App";
 
-let todoItems = [
-  { index: 0, value: "Learn React", done: false },
-  { index: 1, value: "Drink a coffe", done: true },
-  { index: 2, value: "Eat lunch", done: true },
-];
-
 ReactDOM.render(
   <React.StrictMode>
-    <App initItems={todoItems} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/App.js
+++ src/pages/App.js
@@ -8,10 +8,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoItems: this.props.initItems,
+      todoItems: [
+        { index: 0, value: "Learn React", done: false },
+        { index: 1, value: "Drink a coffe", done: true },
+        { index: 2, value: "Eat lunch", done: true },
+      ],
     };
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
+    this.todoDone = this.todoDone.bind(this);
   }
   addItem(todoItem) {
     const newTodo = {
@@ -28,6 +33,17 @@ class App extends Component {
         (item, index) => itemIndex !== index
       ),
     }));
+  }
+  todoDone(itemIndex) {
+    const newData = this.state.todoItems.map((item, index) => {
+      if (itemIndex === index) {
+        return { ...item, done: !item.done };
+      } else {
+        return item;
+      }
+    });
+    this.setState({ todoItems: newData });
+    console.log(this.state.todoItems);
   }
   render() {
     return (
