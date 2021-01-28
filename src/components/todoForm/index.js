a6e5abb6f9ec71d40054d3af3e6a4cfc77979303
Denilson Santos <denilson.avsantos@hotmail.com>
2021-01-27 22:29:23 -0300

    feat:starting the change to functional components and typescript

--- src/components/todoForm/index.js
+++ src/components/todoForm/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { DONE_ITEM, ADD_ITEM } from "../../actions";
 import { connect } from "react-redux";
@@ -11,45 +11,36 @@ const mapDispatchToProps = (dispatch) => ({
   doneItem: (payload) => dispatch({ type: DONE_ITEM, payload }),
 });
 
-class TodoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newItemValue: "",
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
+const TodoForm = ({ addItem }) => {
+  const [itemValue, setItemValue] = useState("");
+
+  const handleChange = (e) => {
     console.log(e.target.value);
-    this.setState({ newItemValue: e.target.value });
-  }
-  handleClick(e) {
-    console.log(this.state.newItemValue);
-    this.props.addItem(this.state.newItemValue);
-  }
-  render() {
-    return (
-      <>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="add a new todo..."
-          onChange={this.handleChange}
-        />
-        <button
-          type="submit"
-          onClick={() => this.props.addItem(this.state.newItemValue)}
-          className="btn btn-default"
-        >
-          Add
-        </button>
-        {/* <button onClick={() => console.log(this.props.itemsReducer)}>
-          asd
-        </button> */}
-      </>
-    );
-  }
-}
+    setItemValue(e.target.value);
+  };
+  const handleClick = () => {
+    console.log(itemValue);
+    addItem(itemValue);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="add a new todo..."
+        value={itemValue}
+        onChange={(e) => handleChange(e)}
+      />
+      <button
+        type="submit"
+        onClick={() => handleClick()}
+        className="btn btn-default"
+      >
+        Add
+      </button>
+    </>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
